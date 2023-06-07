cd94d1c2c0510e0e44900b66c1c5204eac7083a8
Replit user <>
2023-06-07 20:14:35 +0000

    hold

--- pages/api/home/home.state.tsx
+++ pages/api/home/home.state.tsx
@@ -6,45 +6,45 @@ import { PluginKey } from '@/types/plugin';
 import { Prompt } from '@/types/prompt';
 
 export interface HomeInitialState {
-  loading: boolean;
-  lightMode: 'light' | 'dark';
-  messageIsStreaming: boolean;
-  modelError: ErrorMessage | null;
-  models: OpenAIModel[];
-  folders: FolderInterface[];
-  conversations: Conversation[];
-  selectedConversation: Conversation | undefined;
-  currentMessage: Message | undefined;
-  prompts: Prompt[];
-  temperature: number;
-  showChatbar: boolean;
-  showPromptbar: boolean;
-  currentFolder: FolderInterface | undefined;
-  messageError: boolean;
-  searchTerm: string;
-  defaultModelId: OpenAIModelID | undefined;
-  serverSideApiKeyIsSet: boolean;
-  serverSidePluginKeysSet: boolean;
+    loading: boolean;
+    lightMode: 'light' | 'dark';
+    messageIsStreaming: boolean;
+    modelError: ErrorMessage | null;
+    models: OpenAIModel[];
+    folders: FolderInterface[];
+    conversations: Conversation[];
+    selectedConversation: Conversation | undefined;
+    currentMessage: Message | undefined;
+    prompts: Prompt[];
+    temperature: number;
+    showChatbar: boolean;
+    showPromptbar: boolean;
+    currentFolder: FolderInterface | undefined;
+    messageError: boolean;
+    searchTerm: string;
+    defaultModelId: OpenAIModelID | undefined;
+    serverSideApiKeyIsSet: boolean;
+    serverSidePluginKeysSet: boolean;
 }
 
 export const initialState: HomeInitialState = {
-  loading: false,
-  lightMode: 'dark',
-  messageIsStreaming: false,
-  modelError: null,
-  models: [],
-  folders: [],
-  conversations: [],
-  selectedConversation: undefined,
-  currentMessage: undefined,
-  prompts: [],
-  temperature: 1,
-  showPromptbar: true,
-  showChatbar: true,
-  currentFolder: undefined,
-  messageError: false,
-  searchTerm: '',
-  defaultModelId: undefined,
-  serverSideApiKeyIsSet: false,
-  serverSidePluginKeysSet: false,
+    loading: false,
+    lightMode: 'light',
+    messageIsStreaming: false,
+    modelError: null,
+    models: [],
+    folders: [],
+    conversations: [],
+    selectedConversation: undefined,
+    currentMessage: undefined,
+    prompts: [],
+    temperature: 1,
+    showPromptbar: true,
+    showChatbar: true,
+    currentFolder: undefined,
+    messageError: false,
+    searchTerm: '',
+    defaultModelId: undefined,
+    serverSideApiKeyIsSet: false,
+    serverSidePluginKeysSet: false,
 };
